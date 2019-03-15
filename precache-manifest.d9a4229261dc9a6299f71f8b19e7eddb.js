76818b1d80e55f5eec2d86dd444ef81c3d4f5192
juchanhwang <ghkdwncks96@gmail.com>
2019-03-15 17:13:53 +0900

    Updates

---
+++ precache-manifest.d9a4229261dc9a6299f71f8b19e7eddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "838754ca8aacc713bfd4",
+    "url": "/movie_app/static/js/main.26265b48.chunk.js"
+  },
+  {
+    "revision": "5d38434d1bbecf474fa5",
+    "url": "/movie_app/static/js/2.eb7024da.chunk.js"
+  },
+  {
+    "revision": "838754ca8aacc713bfd4",
+    "url": "/movie_app/static/css/main.f9c3ab6e.chunk.css"
+  },
+  {
+    "revision": "f7e303f7a145d9d9bc041084bc840da8",
+    "url": "/movie_app/index.html"
+  }
+];
